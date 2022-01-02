20b0bfc9dd5887ae2ff65916ba52bb4e52c06e32
omarali18 <omarali1814000@gmail.com>
2022-01-02 21:18:37 +0600

    contact page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home/Home/Home';
 import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
+import Contact from './pages/Contact/Contact';
 
 function App() {
   return (
@@ -9,11 +10,9 @@ function App() {
       <Router>
       <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="contact" element={<Contact/>} />
       </Routes>
       </Router>
-      {/* <Home/> */}
-     <h1>this is team prohect</h1>
-     <h2>omar ali</h2>
     </div>
   );
 }
